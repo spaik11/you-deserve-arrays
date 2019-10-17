81eae9070375506a8b29b24e94e9f5b726332c54
Sung Paik <sung.paik@codeimmersives.com>
2019-10-17 15:21:26 -0400

    shortened the array parameter to arr

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-function getFirstItemFrom(array) {
-  return array[0];
+function getFirstItemFrom(arr) {
+  return arr[0];
 }
 
-function getLastItemFrom(array) {
-  return array[array.length - 1];
+function getLastItemFrom(arr) {
+  return arr[arr.length - 1];
 }
 
-function getIndex3(array) {
-  if(array.length >= 4) {
-    return array[3];
-  } else if(array.length < 4) {
-    return array[array.length - 1];
+function getIndex3(arr) {
+  if(arr.length >= 4) {
+    return arr[3];
+  } else if(arr.length < 4) {
+    return arr[arr.length - 1];
   }
 }
 
-function firstItemIsNumber(array) {
-  if(typeof array[0] == 'number') {
+function firstItemIsNumber(arr) {
+  if(typeof arr[0] == 'number') {
     return true;
   } else {
     return false;
   }
 }
 
-function isLongList(array) {
-  if(array.length > 10) {
+function isLongList(arr) {
+  if(arr.length > 10) {
     return true;
-  } else if(array.length === 10) {
+  } else if(arr.length === 10) {
     return true;
   } else {
     return false;
