9e66c980efcbe2239dfa02dcf2c5239acabad2ad
Sung Paik <sung.paik@codeimmersives.com>
2019-10-17 14:31:13 -0400

    completed exercise

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-function getFirstItemFrom() {
-
+function getFirstItemFrom(array) {
+  return array[0];
 }
 
-function getLastItemFrom() {
-
+function getLastItemFrom(array) {
+  return array[array.length - 1];
 }
 
-function getIndex3() {
-
+function getIndex3(array) {
+  if(array.length >= 4) {
+    return array[3];
+  } else if(array.length < 4) {
+    return array[array.length - 1];
+  }
 }
 
-function firstItemIsNumber() {
-
+function firstItemIsNumber(array) {
+  if(typeof array[0] == 'number') {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function isLongList() {
-
+function isLongList(array) {
+  if(array.length > 10) {
+    return true;
+  } else if(array.length === 10) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
